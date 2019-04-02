96933a48b51f94cd303822d43c9905b8526f1249
Tatyana sandanova <ts@theventury.com>
2019-04-02 15:48:18 +0200

    conditional toggle

--- section-3/src/App.js
+++ section-3/src/App.js
@@ -1,44 +1,82 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-	const [personsState, setPersonsState] = useState({
+class App extends Component {
+	state = {
 		persons: [
 			{ name: 'tess', age: 5 },
 			{ name: 'pilar', age: 3 },
 			{ name: 'bea', age: 3 },
 		],
-	});
+		showPersons: false,
+	};
 
-	const switchNameHandler = () => {
-		setPersonsState({
+	switchNameHandler = newName => {
+		this.setState({
 			persons: [
 				{ name: 'tess', age: 5 },
-				{ name: 'pilarito', age: 1 },
+				{ name: newName, age: 1 },
 				{ name: 'bea', age: 1 },
 			],
 		});
 	};
 
-	return (
-		<div className="App">
-			<h1>Hi, I'm a React App!</h1>
-			<button onClick={switchNameHandler}>Switch name</button>
-			<Person
-				name={personsState.persons[0].name}
-				age={personsState.persons[0].age}
-			/>
-			<Person
-				name={personsState.persons[1].name}
-				age={personsState.persons[1].age}
-			/>
-			<Person
-				name={personsState.persons[2].name}
-				age={personsState.persons[2].age}
-			/>
-		</div>
-	);
-};
+	nameChangedhandler = event => {
+		this.setState({
+			persons: [
+				{ name: 'tess', age: 5 },
+				{ name: event.target.value, age: 1 },
+				{ name: 'bea', age: 1 },
+			],
+		});
+	};
 
-export default app;
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+		this.setState({
+			showPersons: !doesShow,
+		});
+	};
+
+	render() {
+		const style = {
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer',
+		};
+
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App!</h1>
+				<button style={style} onClick={this.togglePersonsHandler}>
+					Switch name
+				</button>
+				{this.state.showPersons ? (
+					<div>
+						<Person
+							name={this.state.persons[0].name}
+							age={this.state.persons[0].age}
+						/>
+						<Person
+							name={this.state.persons[1].name}
+							age={this.state.persons[1].age}
+							click={this.switchNameHandler.bind(this, 'pilarka')}
+							changed={this.nameChangedhandler}
+						>
+							Hobby: gaming
+						</Person>
+						<Person
+							name={this.state.persons[2].name}
+							age={this.state.persons[2].age}
+						/>
+					</div>
+				) : null}
+			</div>
+		);
+	}
+}
+
+export default App;
