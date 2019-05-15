a4b6212d75b8d9a793ad9e2ab0896711725f4831
Tatyana sandanova <ts@theventury.com>
2019-05-15 14:42:04 +0200

    revert radium

--- section-3/src/App.js
+++ section-3/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
 
 import './App.css';
 import Person from './Person/Person';
@@ -59,10 +58,6 @@ class App extends Component {
 			border: '1px solid blue',
 			padding: '8px',
 			cursor: 'pointer',
-			':hover': {
-				backgroundColor: 'lightgreen',
-				color: 'black',
-			},
 		};
 
 		let persons = null;
@@ -82,10 +77,6 @@ class App extends Component {
 			);
 
 			style.backgroundColor = 'red';
-			style[':hover'] = {
-				backgroundColor: 'salmon',
-				color: 'black',
-			};
 		}
 
 		const classes = [];
@@ -98,19 +89,17 @@ class App extends Component {
 
 		return (
 			// wrap with StyleRoot to access features like media queries
-			<StyleRoot>
-				<div className="App">
-					<h1>Hi, I'm a React App!</h1>
-					<p className={classes.join(' ')}>This is really working!</p>
-					<button style={style} onClick={this.togglePersonsHandler}>
-						Toggle Persons
-					</button>
-					{persons}
-				</div>
-			</StyleRoot>
+			<div className="App">
+				<h1>Hi, I'm a React App!</h1>
+				<p className={classes.join(' ')}>This is really working!</p>
+				<button style={style} onClick={this.togglePersonsHandler}>
+					Toggle Persons
+				</button>
+				{persons}
+			</div>
 		);
 	}
 }
 
 //wrap export with Radium to use pseudo selectors
-export default Radium(App);
+export default App;
