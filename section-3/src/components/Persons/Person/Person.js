3a64390fd31c94f8444dea062d474aa2e979df75
Tatyana sandanova <ts@theventury.com>
2019-10-21 12:23:36 +0200

    use Aux HOC

--- section-3/src/components/Persons/Person/Person.js
+++ section-3/src/components/Persons/Person/Person.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-
+import Aux from '../../../hoc/Auxiliary';
 import styles from './Person.css';
 
 class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...');
-		return [
-			<p key="i1" onClick={this.props.click}>
-				I am {this.props.name} and I am {this.props.age}yo!
-			</p>,
-			<p key="i2">{this.props.children}</p>,
-			<input
-				key="i3"
-				type="text"
-				value={this.props.name}
-				onChange={this.props.changed}
-			/>,
-		];
+		return (
+			<Aux>
+				<p onClick={this.props.click}>
+					I am {this.props.name} and I am {this.props.age}yo!
+				</p>
+				<p>{this.props.children}</p>
+				<input
+					type="text"
+					value={this.props.name}
+					onChange={this.props.changed}
+				/>
+			</Aux>
+		);
 	}
 }
 
