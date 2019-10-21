024779b684bed45264b2213f1746e7e5d298f8af
Tatyana sandanova <ts@theventury.com>
2019-10-21 10:53:28 +0200

    React.memo

--- section-3/src/components/Cockpit/Cockpit.js
+++ section-3/src/components/Cockpit/Cockpit.js
@@ -7,11 +7,10 @@ const cockpit = props => {
 		// executed for every render cycle incl the first one
 		console.log('[Cockpit.js] useEffect');
 		// http request
-		const timer = setTimeout(() => {
+		setTimeout(() => {
 			alert('saved data!');
 		}, 1000);
 		return () => {
-			clearTimeout(timer);
 			console.log('[Cockpit.js] Cleanup work in useEffect');
 		};
 	}, []);
@@ -29,10 +28,10 @@ const cockpit = props => {
 	}
 
 	const assignedClasses = [];
-	if (props.persons.length <= 2) {
+	if (props.personsLength <= 2) {
 		assignedClasses.push(classes.red);
 	}
-	if (props.persons.length <= 1) {
+	if (props.personsLength <= 1) {
 		assignedClasses.push(classes.bold);
 	}
 
@@ -47,4 +46,4 @@ const cockpit = props => {
 	);
 };
 
-export default cockpit;
+export default React.memo(cockpit);
--- section-3/src/containers/App.js
+++ section-3/src/containers/App.js
@@ -103,7 +103,7 @@ class App extends Component {
 				{this.state.showCockpit ? (
 					<Cockpit
 						title={this.props.appTitle}
-						persons={this.state.persons}
+						personsLength={this.state.persons.length}
 						showPersons={this.state.showPersons}
 						clicked={this.togglePersonsHandler}
 					/>
