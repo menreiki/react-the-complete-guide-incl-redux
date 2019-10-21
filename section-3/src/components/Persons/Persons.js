37232d074f8da48ba1da939c0937cec94bbd27d3
Tatyana sandanova <ts@theventury.com>
2019-10-21 11:55:20 +0200

    PureComponent

--- section-3/src/components/Persons/Persons.js
+++ section-3/src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
 	// static getDerivedStateFromProps(props, state) {
 	// 	console.log('[Persons.js] getDerivedStateFromProps');
 	// 	return state;
@@ -12,14 +12,14 @@ class Persons extends Component {
 	// 	console.log('[Persons.js] componentWillReceiveProps', props);
 	// }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		console.log('[Persons.js] shouldComponentUpdate');
-		return (
-			nextProps.persons !== this.props.persons ||
-			nextProps.changed !== this.props.changed ||
-			nextProps.clicked !== this.props.clicked
-		);
-	}
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	console.log('[Persons.js] shouldComponentUpdate');
+	// 	return (
+	// 		nextProps.persons !== this.props.persons ||
+	// 		nextProps.changed !== this.props.changed ||
+	// 		nextProps.clicked !== this.props.clicked
+	// 	);
+	// }
 
 	getSnapshotBeforeUpdate(prevProps, prevState) {
 		console.log('[Persons.js] getSnapshotBeforeUpdate');
