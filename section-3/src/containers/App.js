9829d84458f143d9f6d34339a9eab0b2f5214391
Tatyana sandanova <ts@theventury.com>
2019-10-21 12:32:40 +0200

    add WithClass HOC

--- section-3/src/containers/App.js
+++ section-3/src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 	constructor(props) {
@@ -96,7 +97,7 @@ class App extends Component {
 
 		return (
 			// wrap with StyleRoot to access features like media queries
-			<div className={classes.App}>
+			<WithClass classes={classes.App}>
 				<button onClick={() => this.setState({ showCockpit: false })}>
 					Remove Cockpit
 				</button>
@@ -109,7 +110,7 @@ class App extends Component {
 					/>
 				) : null}
 				{persons}
-			</div>
+			</WithClass>
 		);
 	}
 }
