c39dfbc4829c6be1949d94a7a453ca6eeb1f7810
Tatyana sandanova <ts@theventury.com>
2019-10-21 12:17:40 +0200

    render components array

--- section-3/src/components/Persons/Person/Person.js
+++ section-3/src/components/Persons/Person/Person.js
@@ -5,19 +5,18 @@ import styles from './Person.css';
 class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...');
-		return (
-			<div className={styles.Person}>
-				<p onClick={this.props.click}>
-					I am {this.props.name} and I am {this.props.age}yo!
-				</p>
-				<p>{this.props.children}</p>
-				<input
-					type="text"
-					value={this.props.name}
-					onChange={this.props.changed}
-				/>
-			</div>
-		);
+		return [
+			<p key="i1" onClick={this.props.click}>
+				I am {this.props.name} and I am {this.props.age}yo!
+			</p>,
+			<p key="i2">{this.props.children}</p>,
+			<input
+				key="i3"
+				type="text"
+				value={this.props.name}
+				onChange={this.props.changed}
+			/>,
+		];
 	}
 }
 
