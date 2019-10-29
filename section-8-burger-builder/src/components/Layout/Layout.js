9bc1d3671ae8ab10f3695f967035289ff491b34c
Tatyana sandanova <ts@theventury.com>
2019-10-29 10:39:16 +0100

    add burger components

--- section-8-burger-builder/src/components/Layout/Layout.js
+++ section-8-burger-builder/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Aux from '../../hoc/Auxiliary'
+import Aux from '../../hoc/Auxiliary';
+import styles from './Layout.module.css'
 
 const layout = props => (
   // or React.Fragment
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={styles.Content}>
       {props.children}
     </main>
   </Aux>
